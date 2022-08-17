98b563506d6ebac459863128b9fd010fd98bf9dd
sanggu239 <romensch39@naver.com>
2022-08-17 13:40:05 +0900

    1. 유저별 ShortenUrl 테이블 생성

--- shortner/accountapp/models.py
+++ shortner/accountapp/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    DateOfBirth = models.DateField(null=True)     # 생년월일
-    Gender      = models.CharField(max_length=2)  # 성별
-    Contact     = models.CharField(max_length=12) # 연락처
-    Payplan     = models.CharField(max_length=2)  # 유료가입
-    Period      = models.DateField(null=True, blank=True)     # 유료가입 기간
+    DateOfBirth = models.DateField(null=True)      # 생년월일
+    Gender      = models.CharField(max_length=2)   # 성별
+    Contact     = models.CharField(max_length=12)  # 연락처
+    Payplan     = models.CharField(max_length=2)   # 유료가입
+    Period      = models.DateField(null=True, blank=True) # 유료가입 기간
+    Url_cnt     = models.IntegerField(blank=False) # 최대 URL 갯수
--- shortner/shortlink/models.py
+++ shortner/shortlink/models.py
@@ -1,9 +1,19 @@
+from tkinter import CASCADE
 from django.db import models
+from accountapp.models import User
+
 import datetime
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Sample(models.Model):
     create_dt = models.DateTimeField(auto_now_add = True)
     orig_url  = models.CharField(max_length=2000)
     redic_url = models.CharField(max_length=2000)
+
+class UserShortenList(TimeStampedModel):
+    user_id     = models.ForeignKey(User, on_delete=models.CASCADE)
+    shorten_url = models.CharField(max_length=2000, blank=False)
